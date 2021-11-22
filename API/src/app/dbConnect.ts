88d761fb7a9f8f82d65a342f33a1229ce08bbcdc
AhmadHddad <AhmadHddad@outlook.com>
2021-11-23 00:50:15 +0200

    setting dbConnect config

---
+++ API/src/app/dbConnect.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+export default async function dbConnect() {
+  const connectionStr = 'mongodb://localhost:27017/test';
+
+  try {
+    await mongoose.connect(connectionStr);
+
+    console.log("DB connected successfully!");
+    
+  } catch (error) {
+
+    throw new Error("Could not connect to DB" + error?.message || error);
+    
+  }
+
+
+}
