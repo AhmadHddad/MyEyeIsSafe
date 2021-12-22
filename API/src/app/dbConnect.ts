261522ddff4611d794b793d8ac0b8a1cf01cd43e
AhmadHddad <AhmadHddad@outlook.com>
2021-12-22 22:25:54 +0200

    adding dbconnect

--- API/src/app/dbConnect.ts
+++ API/src/app/dbConnect.ts
@@ -1,18 +1,16 @@
-import mongoose from "../data/mongoose";
+import mongoose from '../data/mongoose';
 
 export default async function dbConnect() {
-  const connectionStr = 'mongodb://localhost:27017/test';
+  const connectionStr = 'mongodb://localhost:27017/MyEyeIsSafe';
+
+  console.info('Trying to Connect to DB');
 
   try {
     await mongoose.connect(connectionStr);
 
-    console.log("DB connected successfully!");
-    
+    console.log('DB connected successfully!');
+
   } catch (error) {
-
-    throw new Error("Could not connect to DB" + error?.message || error);
-    
+    throw new Error('Could not connect to DB =>' + error?.message || error);
   }
-
-
 }
