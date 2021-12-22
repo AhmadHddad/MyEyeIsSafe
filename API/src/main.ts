6c7f780c54135f45548dd1018238f59d4baed0c5
AhmadHddad <AhmadHddad@outlook.com>
2021-12-22 22:26:50 +0200

    adding main.ts file

--- API/src/main.ts
+++ API/src/main.ts
@@ -1,21 +1,46 @@
-import express, { Application, NextFunction } from 'express';
+import express from 'express';
 import dbConnect from './app/dbConnect';
 import { AllowCors } from './middlewares/allowCores';
+import StudentModel from './models/student';
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.json())
+app.use(express.json());
 app.use(AllowCors);
 
-await dbConnect();
-
-app.listen(port, () => console.log(`Server running on ${port}!`));
+dbConnect().then(() => {
+  app.listen(port, () => console.log(`Server running on ${port}!`));
+});
 
 app.get('/hello/:value', (req, res) => {
   console.log(`${req.method}: ${req.url}`);
   const str = req.params.value as string;
   res.statusCode = 400;
   res.send({ message: `Hello ${str}!` });
+});
+
+
+app.get('/student',async (_,res)=>{
+
+const students = await StudentModel.find();
+
+res.send(students);
+});
+
+
+app.post('/student', async (req, res, next) => {
+  const { name } = req.body;
+
+  
+  const student = await new StudentModel({ name }).save();
+  
+  res.json(student.toJSON());
+});
+
+app.use((req, res, next) => {
+  console.log('NOOOO');
+  res.statusCode = 404;
+  res.json({ message: 'Not Found!' });
 });
